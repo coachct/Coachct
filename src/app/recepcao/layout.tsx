42a0a13ff4723c2d69edf85c06d606e28f99f72a
coachct <ricardo@justclub.com.br>
2026-05-26 15:55:42 -0300

    Update layout.tsx

--- src/app/recepcao/layout.tsx
+++ src/app/recepcao/layout.tsx
@@ -30,7 +30,7 @@ export default function RecepcaoLayout({ children }: { children: React.ReactNode
 
   return (
     <div style={{ display:'flex', minHeight:'100vh', background:'#f3f4f6' }}>
-      <SidebarRecepcao perfil={perfil} />
+      <SidebarRecepcao />
       <div style={{ marginLeft:200, flex:1, minHeight:'100vh' }}>
         {children}
       </div>
